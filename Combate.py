b82739baac9ed2c645518cba65055626ee5c3baf
danimart <daniel.bernaldo@digital55.com>
2024-09-10 12:31:03 +0200

    Añadido efecto typing

--- Combate.py
+++ Combate.py
@@ -3,7 +3,16 @@ from Personaje import Personaje
 from Razas.Sayajin import Sayajin
 from Razas.Namekiano import Namekiano
 
-print("Estos son los personajes disponibles:")
+import time
+
+def mostrar_mensaje_pausado(mensaje, pausa=0.02):
+    for letra in mensaje:
+        print(letra, end='', flush=True)
+        time.sleep(pausa)
+    print()  # Para añadir un salto de línea después de imprimir el mensaje
+
+
+mostrar_mensaje_pausado("Estos son los personajes disponibles:")
 # PERSONAJES
 goku = Sayajin("Goku", 150, 100, 80, 150, "sayajin")
 piccolo = Namekiano("Piccolo", 200, 90, 60, 120, "namekiano")
@@ -21,14 +30,14 @@ equipo1.append(krillin)
 equipo2.append(vegeta)
 equipo2.append(frieza) 
 
-print("Equipo 1:")
+mostrar_mensaje_pausado("Equipo 1:")
 for personaje in equipo1:
-    print(f"{personaje.nombre}")
+    mostrar_mensaje_pausado(f"{personaje.nombre}")
 
-print("Equipo 2:")
+mostrar_mensaje_pausado("Equipo 2:")
 
 for personaje in equipo2:
-    print(f"{personaje.nombre}")
+    mostrar_mensaje_pausado(f"{personaje.nombre}")
 
 nuevo= input("¿Deseas añadir más personajes a los equipos? (s/n): ")
 
@@ -37,8 +46,8 @@ if nuevo == "s" or nuevo == "si":
 else:
     otro = False
 
-print("Vamos a crear un personaje")
 def crear():
+    mostrar_mensaje_pausado("Vamos a crear un personaje")
     nombre = input("Introduce el nombre del personaje: ")
     
     vida = input("Cuánta vida tiene de 0 a 200 (default: 100): ")
@@ -77,22 +86,22 @@ def crear():
             personaje = Namekiano(nombre, vida, ataque, ki, defensa, raza)
             break
         else:
-            print("Raza no válida. Por favor, elige una raza válida.")
+            mostrar_mensaje_pausado("Raza no válida. Por favor, elige una raza válida.")
     
-    print("Has creado este personaje:")
-    print(f"Nombre: {personaje.nombre}")
-    print(f"Vida: {personaje.vida}")
-    print(f"Ataque: {personaje.ataque}")
-    print(f"Ki: {personaje.ki}")
-    print(f"Defensa: {personaje.defensa}")
-    print(f"Raza: {personaje.raza}")
+    mostrar_mensaje_pausado("Has creado este personaje:")
+    mostrar_mensaje_pausado(f"Nombre: {personaje.nombre}")
+    mostrar_mensaje_pausado(f"Vida: {personaje.vida}")
+    mostrar_mensaje_pausado(f"Ataque: {personaje.ataque}")
+    mostrar_mensaje_pausado(f"Ki: {personaje.ki}")
+    mostrar_mensaje_pausado(f"Defensa: {personaje.defensa}")
+    mostrar_mensaje_pausado(f"Raza: {personaje.raza}")
     
     if len(equipo1) <= len(equipo2):
         equipo1.append(personaje)
-        print(f"Añadido {personaje.nombre} al equipo 1")
+        mostrar_mensaje_pausado(f"Añadido {personaje.nombre} al equipo 1")
     else:
         equipo2.append(personaje)
-        print(f"Añadido {personaje.nombre} al equipo 2")
+        mostrar_mensaje_pausado(f"Añadido {personaje.nombre} al equipo 2")
 
     continuar = input("¿Deseas crear otro personaje para el otro equipo? (si/no): ").lower()
     return continuar in ["si", "sí"]
@@ -100,16 +109,16 @@ def crear():
 while otro:
     otro = crear()
         
-print("Empecemos el combate")
-print("Se enfrentará el Equipo 1, formado por:")
+mostrar_mensaje_pausado("Empecemos el combate")
+mostrar_mensaje_pausado("Se enfrentará el Equipo 1, formado por:")
 
 for personaje in equipo1:
-    print(f"{personaje.nombre}")
+    mostrar_mensaje_pausado(f"{personaje.nombre}")
 
-print("Contra el Equipo 2, formado por:")
+mostrar_mensaje_pausado("Contra el Equipo 2, formado por:")
 
 for personaje in equipo2:
-    print(f"{personaje.nombre}")
+    mostrar_mensaje_pausado(f"{personaje.nombre}")
 
 # LÓGICA DE COMBATE
 
@@ -119,10 +128,10 @@ def combate():
         defensor2 = random.choice(equipo2)
         atacante1.atacar(defensor2)
         
-        print(f"{atacante1.nombre} ha atacado a {defensor2.nombre} y ahora le quedan {defensor2.vida} pts de vida")
+        mostrar_mensaje_pausado(f"{atacante1.nombre} ha atacado a {defensor2.nombre} y ahora le quedan {defensor2.vida} pts de vida")
         
         if defensor2.vida <= 0:
-            print(f"{defensor2.nombre} ha sido derrotado")
+            mostrar_mensaje_pausado(f"{defensor2.nombre} ha sido derrotado")
             equipo2.remove(defensor2)
         else:
             if isinstance(atacante1, Namekiano)and random.random() < 0.5:
@@ -137,10 +146,10 @@ def combate():
         defensor1 = random.choice(equipo1)
         atacante2.atacar(defensor1)
         
-        print(f"{atacante2.nombre} ha atacado a {defensor1.nombre} y ahora le quedan {defensor1.vida} pts de vida")
+        mostrar_mensaje_pausado(f"{atacante2.nombre} ha atacado a {defensor1.nombre} y ahora le quedan {defensor1.vida} pts de vida")
         
         if defensor1.vida <= 0:
-            print(f"{defensor1.nombre} ha sido derrotado")
+            mostrar_mensaje_pausado(f"{defensor1.nombre} ha sido derrotado")
             equipo1.remove(defensor1)
         else:
             if isinstance(atacante2, Namekiano)and random.random() < 0.5:
@@ -149,8 +158,8 @@ def combate():
                 atacante2.transformar()
             
     if len(equipo1) > 0:
-        print("Gana el equipo 1")
+        mostrar_mensaje_pausado("Gana el equipo 1")
     else:
-        print("Gana el equipo 2")
+        mostrar_mensaje_pausado("Gana el equipo 2")
 
 combate()
